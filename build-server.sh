607e540b9ee952827b86a584c3867381a1eb7168
Tobias Macey <tmacey@boundlessnotions.com>
2014-09-03 20:03:07 -0400

    Switched back to using v2014.1.10 due to freezing issues with salt-master

--- build-server.sh
+++ build-server.sh
@@ -3,12 +3,11 @@
 if [ -z `which salt-minion` ]
 then
     curl -o install_salt.sh -L https://bootstrap.saltstack.com
-    sudo sh install_salt.sh -M git develop
+    sudo sh install_salt.sh -M git v2014.1.10
 fi
 sudo mkdir -p /srv/salt
 sudo mkdir -p /srv/pillar
 sudo mkdir -p /srv/formulas
 sudo cp -r salt/* /srv/salt
 sudo cp -r pillar/* /srv/pillar
-sudo cp minion.conf /etc/salt/minion.d/minion.conf
 sudo salt-call --local state.highstate
